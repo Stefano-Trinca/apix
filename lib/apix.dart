037f088e6d901132ab7b4a4378ce87e162b571d0
Stefano-Trinca <ste.developer98@gmail.com>
2024-04-04 14:19:47 +0200

    [add] ext

--- lib/apix.dart
+++ lib/apix.dart
@@ -1,7 +1,24 @@
 library apix;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+/// io
+export 'src/io/directory.dart';
+export 'src/io/file.dart';
+export 'src/io/file_system_entity.dart';
+
+//
+//
+export 'src/arithmetic.dart';
+export 'src/comparable.dart';
+export 'src/comparator.dart';
+export 'src/function.dart';
+export 'src/int.dart';
+export 'src/iterable.dart';
+export 'src/iterable_num.dart';
+export 'src/list.dart';
+export 'src/map.dart';
+export 'src/num.dart';
+export 'src/range.dart';
+export 'src/sorted_list.dart';
+export 'src/string.dart';
+
+
